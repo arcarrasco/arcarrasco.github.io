b66c0234b5da1d9eafa72aba415a29ae6311c903
arcarrasco <126635406+arcarrasco@users.noreply.github.com>
2023-03-07 22:58:07 -0300

    initial commit

---
+++ formulario.js
@@ -0,0 +1,9 @@
+
+function enviar (){
+    var nombre=document.getElementById("name").value;
+    var correo=document.getElementById("email").value;
+    var celular=document.getElementById("cel").value;
+    var texto=document.getElementById("mensaje").value;
+    console.log(nombre +" "+correo +" "+celular + " "+texto);
+}
+
